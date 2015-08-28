1c3222ada88540c2fbc96aaab42d201a09b256db
Tim Gieseking <timstl@gmail.com>
2015-08-28 13:58:29 -0500

    fully working, needs testing

--- class.gfaddressdropdowns.php
+++ class.gfaddressdropdowns.php
@@ -81,6 +81,7 @@ class GFAddressDropdowns extends GFAddOn {
 	public function init_frontend(){
 		parent::init_frontend();
 		add_action('gform_enqueue_scripts', array($this, 'enqueue_scripts'), 10, 2);
+		add_filter( 'gform_pre_render', array($this, 'add_form_class') );
 	}
 
 	public function init_ajax(){
@@ -88,7 +89,25 @@ class GFAddressDropdowns extends GFAddOn {
 	}
 	
 	public function enqueue_scripts( $form = '', $is_ajax = false ) {
+		if (wp_script_is( 'gfad', 'enqueued' )) {
+			return; 
+		}
+		
+		$gfad = array('states' => GFCommon::get_us_states(), 'provinces' => GFCommon::get_canadian_provinces());
 		wp_enqueue_script( 'gfad', $this->get_base_url() . '/js/gfad.js',  array('jquery'),  $this->_version, true);
+		wp_localize_script( 'gfad', 'gfad_regions', $gfad );
+	}
+
+	public function add_form_class( $form ) {
+		
+		if (! empty( $form['cssClass'])) { 
+			$form['cssClass'] .= ' gfad_form';
+		} else {
+			$form['cssClass'] = 'gfad_form';
+			
+		}
+		
+		return $form;
 	}
 }
 }
