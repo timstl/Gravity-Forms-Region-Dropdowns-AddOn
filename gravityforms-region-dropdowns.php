3f3ed00c8b867ad2a1a2734f229224538c44d35f
Tim Gieseking <timstl@gmail.com>
2015-09-14 14:09:39 -0500

    add GPL

--- gravityforms-region-dropdowns.php
+++ gravityforms-region-dropdowns.php
@@ -1,13 +1,28 @@
 <?php
 /*
  Plugin Name: Gravity Forms Region Dropdowns AddOn
- Plugin URI: http://timgweb.com/gf-address-dropdowns/
  Description: Plugin to insert state and province dropdowns for US and CA into Gravity Forms international address fields.
  Version: 1.0
- Author: Tim Gieseking
- Author URI: http://timgweb.com
+ Author: Atomicdust
+ Author URI: http://atomicdust.com
  Text Domain: gf-address-dropdowns
- */
+ 
+------------------------------------------------------------------------
+Copyright 2015 Atomicdust
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses.
+*/
  
 define( 'GF_ADDRESS_DROPDOWNS_VER', '1.0' );
 
